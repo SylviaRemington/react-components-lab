68395d3bf018566f531830e16ee76e5f2e9afdf1
SylviaRemington <sylviaremington@gmail.com>
2025-07-22 16:10:14 -0700

    lesson mvp done & playing around with adding addtl components for fun but commented them out

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -3,6 +3,7 @@ import './WeatherForecast.css'
 const WeatherForecast = (props) => {
     return (
         <div className="weather">
+            {/* I added 'Day of the Week:' verbiage just for something different. */}
             <h2>Day of the Week: {props.weatherForecast.day}</h2>
             <img src={props.weatherForecast.img} alt={props.weatherForecast.imgAlt} />
             <p><span>conditions: </span>{props.weatherForecast.conditions}</p>
